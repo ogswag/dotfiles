e3946ce2d1001441b276167534484978989b20db
admin <alexz@ghost.local>
2024-09-14 12:50:48 +0300

    update

--- nvim/init.lua
+++
@@ -1,5 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-
-vim.cmd("language en_US")
-
-require("config.lazy")
--- nvim/lua/plugins/themes.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "f-person/auto-dark-mode.nvim",
-  "gbprod/nord.nvim",
-  "catppuccin/nvim",
-  requires = { "rktjmp/lush.nvim" },
-}
