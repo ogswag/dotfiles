e48c2ec76f57d91a7a9b9a46acf63496f8fba111
alee3x <alex@zxrv.ru>
2024-11-01 03:52:52 +0300

    update nvim

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,45 +1,43 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	config = function()
-		require("catppuccin").setup({
-			custom_highlights = function(colors)
-				return {
-					Comment = { fg = "#9DCD00" },
-					TabLineSel = { bg = colors.pink },
-					CmpBorder = { fg = colors.surface2 },
-					Pmenu = { bg = colors.none },
-					Visual = { bg = colors.peach, fg = colors.base },
-					StatusLine = { bg = "#3A3A5A", fg = "#CDD6F4" },
-				}
-			end,
-			transparent_background = false, -- disables setting the background color.
-			show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-			term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-			dim_inactive = {
-				enabled = true, -- dims the background color of inactive window
-				shade = "dark",
-				percentage = 0.15, -- percentage of the shade to apply to the inactive window
-			},
-			-- no_italic = false, -- Force no italic
-			-- no_bold = false, -- Force no bold
-			-- no_underline = false, -- Force no underline
-			styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-				comments = {}, -- Change the style of comments
-				conditionals = {},
-				loops = {},
-				functions = {},
-				keywords = {},
-				strings = {},
-				variables = {},
-				numbers = {},
-				booleans = {},
-				properties = {},
-				types = {},
-				operators = {},
-				-- miscs = {}, -- Uncomment to turn off hard-coded styles
-			},
-		})
-	end,
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+        require("catppuccin").setup({
+            custom_highlights = function(colors)
+                return {
+                    Comment = { fg = "#9DCD00" },
+                    Visual = { bg = colors.rosewater, fg = colors.base },
+                    StatusLine = { bg = "#3A3A5A", fg = "#CDD6F4" },
+                    TabLineSel = { bg = colors.lavender, fg = colors.base }
+                }
+            end,
+            transparent_background = false, -- disables setting the background color.
+            show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
+            term_colors = false,            -- sets terminal colors (e.g. `g:terminal_color_0`)
+            dim_inactive = {
+                enabled = true,             -- dims the background color of inactive window
+                shade = "dark",
+                percentage = 0.15,          -- percentage of the shade to apply to the inactive window
+            },
+            -- no_italic = false, -- Force no italic
+            -- no_bold = false, -- Force no bold
+            -- no_underline = false, -- Force no underline
+            styles = {         -- Handles the styles of general hi groups (see `:h highlight-args`):
+                comments = {}, -- Change the style of comments
+                conditionals = {},
+                loops = {},
+                functions = {},
+                keywords = {},
+                strings = {},
+                variables = {},
+                numbers = {},
+                booleans = {},
+                properties = {},
+                types = {},
+                operators = {},
+                -- miscs = {}, -- Uncomment to turn off hard-coded styles
+            },
+        })
+    end,
 }
---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,35 @@
+return {
+    "williamboman/mason.nvim",
+    dependencies = {
+        "williamboman/mason-lspconfig.nvim",
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
+    config = function()
+        require("mason").setup()
+
+        require("mason-lspconfig").setup({
+            automatic_installation = true,
+            ensure_installed = {
+                "cssls",
+                "eslint",
+                "html",
+                "jsonls",
+                "ts_ls",
+                "pyright",
+                "clangd",
+                "tailwindcss",
+                "lua_ls",
+            },
+        })
+
+        require("mason-tool-installer").setup({
+            ensure_installed = {
+                "prettier",
+                "stylua", -- lua formatter
+                "black", -- python formatter
+                "pylint",
+                "eslint_d",
+            },
+        })
+    end,
+}
