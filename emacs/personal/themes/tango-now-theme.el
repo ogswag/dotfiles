2fdb5f0ca9245f37ffad70622d6d6c857e4a1835
alee3x <sneed@disroot.org>
2024-11-15 22:21:28 +0300

    emacs additions

--- emacs/personal/themes/tango-now-theme.el
+++ emacs/personal/themes/tango-now-theme.el
@@ -264,8 +264,9 @@
   (when (>= emacs-major-version 26)
     (custom-theme-set-faces
      'tango-now
-     `(line-number ((t (:inherit fringe))))
-     `(line-number-current-line ((t (:background "#4f5456" :foreground "misty rose"))))))
+
+     `(line-number ((t (:inherit default))))
+     `(line-number-current-line ((t (:inherit default :background "#4f5456" :foreground "seashell"))))))
   ;; emacs >= 27.1
   (when (>= emacs-major-version 27)
     (custom-theme-set-faces
