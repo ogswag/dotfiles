bad985f0fab6506d6b058ab16c43636bee9a87b0
ogswag <apsacharow@mail.ru>
2026-06-05 13:41:34 +0300

    tokyonight theme for neovim + neovim zsh_aliases for linux

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,26 +1,34 @@
 return {
-	{
-		"kungfusheep/mfd.nvim",
-		lazy = false,
-	},
-	{
-		"IroncladDev/osmium",
-		lazy = false,
-		config = function()
-			require("osmium").setup({
-				integrations = {
-					gitsigns = true,
-					telescope = true,
-				},
-				transparent_bg = false,
-				show_end_of_buffer = false,
-			})
-			vim.cmd([[colors osmium]])
-		end,
-	},
-	{
-		"wtfox/jellybeans.nvim",
-		lazy = false,
-		priority = 1000,
-	},
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd [[colorscheme tokyonight]]
+        end,
+        opts = {},
+    },
+    {
+        "kungfusheep/mfd.nvim",
+        lazy = false,
+    },
+    {
+        "IroncladDev/osmium",
+        lazy = false,
+        config = function()
+            require("osmium").setup({
+                integrations = {
+                    gitsigns = true,
+                    telescope = true,
+                },
+                transparent_bg = false,
+                show_end_of_buffer = false,
+            })
+        end,
+    },
+    {
+        "wtfox/jellybeans.nvim",
+        lazy = false,
+        priority = 1000,
+    },
 }
