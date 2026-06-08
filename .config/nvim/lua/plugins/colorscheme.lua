8a17c46ed63375e9d0dceddd6165581203cbc73d
ogswag <irlpilled@gmail.com>
2026-06-08 15:56:22 +0300

    colorscheme changes in neovim

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,48 @@
 return {
+	{
+		"ogswag/valve-olive.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			darker_bg = false,
+			transparent = false,
+			italic_comments = true,
+			bold_functions = true,
+		},
+		config = function(_, opts)
+			require("valve-olive").setup(opts)
+			vim.cmd.colorscheme("valve-olive")
+		end,
+	},
+	{
+		"dchinmay2/alabaster.nvim",
+		lazy = false,
+		priority = 1000,
+	},
+	{ "oonamo/ef-themes.nvim", lazy = false, priority = 1000 },
 	{ "ogswag/vim-envy", lazy = false, priority = 1000 },
+	{ "NLKNguyen/papercolor-theme", lazy = false, priority = 1000 },
+	{ "jonathanfilip/vim-lucius", lazy = false, priority = 1000 },
+	{
+		"zenbones-theme/zenbones.nvim",
+		lazy = false,
+		-- you can set set configuration options here
+		config = function()
+			vim.g.zenbones_compat = 1
+		end,
+	},
+	{
+		"miikanissi/modus-themes.nvim",
+		priority = 1000,
+		config = function()
+			require("modus-themes").setup({
+				variants = {
+					modus_operandi = "tinted", -- Set variant for `modus_operandi` style
+					modus_vivendi = "default", -- Set variant for `modus_vivendi` style
+				},
+			})
+		end,
+	},
 	{
 		"forest-nvim/sequoia.nvim",
 		lazy = false,
@@ -43,11 +86,11 @@ return {
 			update_interval = 3000,
 			set_dark_mode = function()
 				vim.o.background = "dark"
-				vim.cmd.colorscheme("darkblue")
+				vim.cmd.colorscheme("valve-olive")
 			end,
 			set_light_mode = function()
 				vim.o.background = "light"
-				vim.cmd.colorscheme("envy")
+				vim.cmd.colorscheme("zenbones")
 			end,
 		},
 	},
